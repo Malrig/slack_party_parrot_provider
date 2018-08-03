b66650fb50a67f57bedb08dccf04435350f22d85
Malrig <g-a-b@hotmail.co.uk>
2018-08-03 17:02:00 +0100

    Begin work on the party blame functionality

--- src/parrot_blame.py
+++ src/parrot_blame.py
@@ -1,21 +1,48 @@
+from datetime import datetime
+from collections import namedtuple
+
+ParrotBlameInfo = namedtuple('ParrotBlameInfo', [
+    "parrot_name",
+    "username",
+    "team_name",
+    "created_date"
+])
+
+
 class ParrotBlame:
     def __init__(self,
                  parrot_blame_filename: str):
         self.parrot_blame_filename = parrot_blame_filename
         print("ParrotBlame")
 
-    def add_party_parrot_information(self,
+    def add_parrot_blame_information(self,
                                      parrot_name: str,
                                      username: str,
                                      team_name: str):
-        print("add_party_parrot_information")
+        current_info = self._get_parrot_blame_information()
+
+        current_info.append(ParrotBlameInfo(parrot_name,
+                                            username,
+                                            team_name,
+                                            datetime.now()))
+
+        self._save_parrot_blame_information(current_info)
 
     def blame_parrot(self,
-                     parrot_name: str):
-        print("blame_parrot")
+                     parrot_name: str,
+                     team_name: str):
+        blame_info = self._get_parrot_blame_information()
+
+        for blame_entry in blame_info:
+            if ((blame_entry["parrot_name"] == parrot_name) and
+                    (blame_entry["team_name"] == team_name)):
+                return blame_entry
+
+        raise ValueError("The parrot :{parrot_name}: was not found in the blame information."
+                            .format(parrot_name=parrot_name))
 
     def _get_parrot_blame_information(self):
         print("_get_parrot_blame_information")
 
-    def _add_parrot_blame_information(self):
+    def _save_parrot_blame_information(self, new_info: list):
         print("_add_parrot_blame_information")
