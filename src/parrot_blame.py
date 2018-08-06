82462ea378a2e0cb465e746cb86f4e0f151e857d
Home Computer\George <g-a-b@hotmail.co.uk>
2018-08-06 20:29:16 +0100

    Merge changes across multiple computers

--- src/parrot_blame.py
+++ src/parrot_blame.py
@@ -1,11 +1,12 @@
+import json
 from datetime import datetime
 from collections import namedtuple
 
-ParrotBlameInfo = namedtuple('ParrotBlameInfo', [
+ParrotBlameInfo = namedtuple('ParrotBlameObj', [
     "parrot_name",
     "username",
-    "team_name",
-    "created_date"
+    "created_date",
+    "team_id"
 ])
 
 
@@ -13,29 +14,28 @@ class ParrotBlame:
     def __init__(self,
                  parrot_blame_filename: str):
         self.parrot_blame_filename = parrot_blame_filename
-        print("ParrotBlame")
 
     def add_parrot_blame_information(self,
                                      parrot_name: str,
                                      username: str,
-                                     team_name: str):
+                                     team_id: str):
         current_info = self._get_parrot_blame_information()
 
         current_info.append(ParrotBlameInfo(parrot_name,
                                             username,
-                                            team_name,
+                                            team_id,
                                             datetime.now()))
 
         self._save_parrot_blame_information(current_info)
 
     def blame_parrot(self,
                      parrot_name: str,
-                     team_name: str):
+                     team_id: str):
         blame_info = self._get_parrot_blame_information()
 
         for blame_entry in blame_info:
             if ((blame_entry["parrot_name"] == parrot_name) and
-                    (blame_entry["team_name"] == team_name)):
+                    (blame_entry["team_id"] == team_id)):
                 return blame_entry
 
         raise ValueError("The parrot :{parrot_name}: was not found in the blame information."
@@ -44,5 +44,6 @@ class ParrotBlame:
     def _get_parrot_blame_information(self):
         print("_get_parrot_blame_information")
 
-    def _save_parrot_blame_information(self, new_info: list):
+    def _add_parrot_blame_information(self,
+                                      parrot_blame_info: list):
         print("_add_parrot_blame_information")
--- test/test_parrot_blame.py
+++ test/test_parrot_blame.py
@@ -1,9 +1,24 @@
 import unittest
 from unittest.mock import MagicMock
-from src.parrot_blame import ParrotBlame
+from src.parrot_blame import ParrotBlame, ParrotBlameInfo
+
+parrot_blame_data = [
+    {
+        "parrot_name": "joy_parrot",
+        "username": "TestUser",
+        "created_date": "2018-07-24 20:35:43.687369",
+        "team_id": "T001"
+    },
+    {
+        "parrot_name": "sweat_smile_parrot",
+        "username": "AnotherUser",
+        "created_date": "2018-07-10 20:35:43.687369",
+        "team_id": "T003"
+    }
+]
 
 
-class TestParrotBlame(unittest.TestCase):
+class TestParrotBlameWithFile(unittest.TestCase):
     def setUp(self):
         print("setUp")
 
@@ -20,12 +35,36 @@ class TestParrotBlame(unittest.TestCase):
         print("test_override_blame_entry")
 
 
-class TestInitiationParrotBlame(unittest.TestCase):
+class TestParrotBlameWithoutFile(unittest.TestCase):
     def setUp(self):
         print("setUp")
+        self.parrot_blame = ParrotBlame("file_path")
+        self.parrot_blame._get_parrot_blame_information = MagicMock(return_value=parrot_blame_data)
+        self.parrot_blame._add_parrot_blame_information = MagicMock()
 
     def test_initiation_creates_file(self):
         print("test_initiation_creates_file")
 
     def test_initiation_doesnt_override_existing_file(self):
         print("test_initiation_doesnt_override_existing_file")
+
+    def test_blame_parrot(self):
+        blame_parrot = self.parrot_blame.blame_parrot("joy_parrot",
+                                                      "T001")
+
+        self.assertEqual(ParrotBlameInfo(parrot_name="joy_parrot",
+                                         username="TestUser",
+                                         created_date="2018-07-24 20:35:43.687369",
+                                         team_id="T001"),
+                         blame_parrot)
+        self.parrot_blame._get_parrot_blame_information.assert_called()
+
+    def test_blame_parrot_without_entry(self):
+        self.assertRaises(ValueError, self.parrot_blame.blame_parrot, "sweat_smile_parrot", "T001")
+        self.parrot_blame._get_parrot_blame_information.assert_called()
+
+    def test_create_blame_entry(self):
+        print("test_create_blame_entry")
+
+    def test_override_blame_entry(self):
+        print("test_override_blame_entry")
